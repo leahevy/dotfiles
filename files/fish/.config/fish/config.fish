c54ed58d5b731488ed7583b6d8dec0d16cc1499b
Leah Lackner <leah.lackner+github@gmail.com>
2022-04-18 15:24:10 +0200

    Bash and Fish base config

---
+++ files/fish/.config/fish/config.fish
@@ -0,0 +1,25 @@
+
+
+if command -v pyenv &> /dev/null
+    status is-login; and pyenv init --path | source
+    status is-interactive; and pyenv init - | source
+    alias python python3
+end
+
+if command -v rbenv &> /dev/null
+    status --is-interactive; and rbenv init - fish | source
+end
+
+if command -v git &> /dev/null
+    function g
+        if not set -q argv[1]
+            git status
+        else
+            git $argv
+        end
+    end
+end
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
