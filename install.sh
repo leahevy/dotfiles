c056585782a84bb52e235cbb465902ec29a17995
Leah Lackner <leah.lackner+github@gmail.com>
2022-06-08 19:44:08 +0200

    Improve installer

--- install.sh
+++ install.sh
@@ -1,18 +1,41 @@
 #!/bin/bash
 set -euo pipefail
 
+SCRIPT=$(readlink -f $0)
+SCRIPTPATH=`dirname $SCRIPT`
+cd "$SCRIPTPATH"
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+function echo() {
+    printf "${GREEN}>>> ${RED}${@}${NC}\n"
+    printf "${GREEN}"
+	printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+	printf "${NC}"
+}
+
+function fatal() {
+    printf "${RED}"
+	printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+	printf "${NC}"
+    printf "${RED}!!! ${RED}${@}!${NC}\n" >&2
+	exit 1
+}
+
+echo "Installing dotfiles ($SCRIPTPATH)"
+
+if ! command -v pip &> /dev/null; then
+	fatal "pip not found on system"
+fi
+
+echo "Installing mydotfiles Python package locally"
+pip install -e .
+
+echo "Linking dotfiles directory (Run 'mydotfiles' afterwards)"
 if [[ ! -e "$HOME/.dotfiles" ]]; then
     ln -s "$(pwd)" "$HOME/.dotfiles"
 fi
-
-PYTHON_VERSION=3.10.3
-RUBY_VERSION=3.1.2
-
-SCRIPT=$(readlink -f $0)
-SCRIPTPATH=`dirname $SCRIPT`
-
-echo "mydotfiles install script"
-echo
 
 OS="`uname`"
 
@@ -23,132 +46,81 @@ case $OS in
     'Linux')
         OS="linux"
         ;;
+    *)
+        fatal "Invalid operating system found"
+        ;;
 esac
 
 echo "Found operating system $OS"
-echo "Starting installation"
-echo
 
-case $OS in
-    'linux')
-        if ! command -v apt-get &> /dev/null; then
-            echo "Linux found, however, only works in Debian based systems for now."
-            exit 2
-        fi
-        install() {
-            echo "Installing $@"
-            if [ "$(dpkg --list | grep "$1 ")" = "" ]; then
-                DEBIAN_FRONTEND=noninteractive sudo apt-get install -qq "$@"
-                echo
-            fi
-        }
-        finalize() {
-            echo "Cleaning up packages"
-            DEBIAN_FRONTEND=noninteractive sudo apt-get -qq autoremove
-            echo
-        }
-        DEBIAN_FRONTEND=noninteractive sudo apt-get update
-        DEBIAN_FRONTEND=noninteractive sudo apt-get -qq upgrade
-        ;;
-    'osx') 
-        finalize() {
-            echo "Cleaning up brew"
-            brew cleanup
-            echo
-        }
-        if ! command -v brew &> /dev/null; then
-            echo "Installing brew"
-            sudo /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-            echo
-        else
-            echo "Updating brew"
-            brew update
-            echo
-
-            echo "Upgrading packages"
-            brew upgrade
-            echo
-        fi
-        ;;
-    *)
-        echo "Install only works on Linux and MacOS"
-        exit 1
-        ;;
-esac
-
-linux_packages=(
-    coreutils
-    moreutils
-    findutils
-    bash
-    wget
-    gnupg
-    grep
-    screen
-    ack
-    git
-    git-lfs
-    gs
-    python3
-    htop
-    fish
-    ruby
-    fontconfig
-    openssl
-    openvpn
-    readline
-    sqlite3
-    npm
-    curl
-    subversion
-    bash-completion
-    vim
-    texlive
-    docker
-    emacs
-    make
-    openssh-client
-    build-essential
-    libssl-dev
-    zlib1g-dev
-    libbz2-dev
-    libreadline-dev
-    libsqlite3-dev
-    wget
-    curl
-    llvm
-    libncurses5-dev
-    xz-utils
-    tk-dev
-    libxml2-dev
-    libxmlsec1-dev
-    libffi-dev
-    liblzma-dev
-)
-
-# Only run if we are on Linux desktop (not server or Mac)
-if dpkg -l 2>/dev/null | grep xserver-xorg-core >/dev/null 2>&1; then
-    linux_packages+=(
-        firefox
-    )
+if [ "$OS" == "linux" ]; then
+    if ! command -v apt-get &> /dev/null; then
+        fatal "Linux found, however, only works in Debian based systems for now."
+    fi
 fi
 
-echo "Passwords might be requested during the installation"
-echo
+if [ "$OS" == "linux" ]; then
+    readarray -t linux_packages < linux.packages
+    if dpkg -l 2>/dev/null | grep xserver-xorg-core >/dev/null 2>&1; then
+        readarray -t linux_desktop_packages < linux-desktop.packages
+        linux_packages+=( "${linux_desktop_packages[@]}" )
+    fi
+fi
 
-if [ "$OS" == "osx" ]; then
-	brew bundle
-elif [ "$OS" == "linux" ]; then
+echo "Starting installation\nPasswords might be requested during the installation"
+
+echo "Check for Homebrew installation"
+if ! command -v brew &> /dev/null; then
+    echo "Installing brew"
+    sudo /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+    echo "Updating brew packages"
+    brew update
+
+    echo "Upgrading brew packages"
+    brew upgrade
+fi
+
+if [ "$OS" == "linux" ]; then
+    echo "Updating linux packages"
+    DEBIAN_FRONTEND=noninteractive sudo apt-get update
+
+    echo "Upgrading linux packages"
+    DEBIAN_FRONTEND=noninteractive sudo apt-get -qq upgrade
+fi
+
+
+if [ "$OS" == "linux" ]; then
+    echo "Installing linux packages"
     for package in "${linux_packages[@]}"; do
         package_array=($package)
-        install "${package_array[@]}"
+        DEBIAN_FRONTEND=noninteractive sudo apt-get install -qq "${package_array[@]}"
     done
 fi
 
-finalize
+echo "Installing packages using Homebrew"
+mkdir -p .tmp
+cp Brewfile .tmp/
+if [ "$OS" == "linux" ]; then
+    sed -i '' '/^cask /d' .tmp/Brewfile
+    sed -i '' '/^mas /d' .tmp/Brewfile
+fi
+(cd .tmp; brew bundle)
+
+echo "Cleaning up old brew packages"
+brew cleanup
+
+if [ "$OS" == "linux" ]; then
+    echo "Cleaning up old linux packages"
+    DEBIAN_FRONTEND=noninteractive sudo apt-get -qq autoremove
+fi
 
 if [ "$OS" == "osx" ]; then
-    "$SCRIPTPATH/macos.sh"
+    echo "MacOS defaults"
+    read -p "Configure? [y?]" answer
+    if [[ "$answer" == "y" ]]; then
+        ./macos.sh
+    fi
 fi
 
 echo "Everything done"
