a0fd5041c48c59c1a16573b5df36045a02068ac7
Leah Lackner <leah.lackner+github@gmail.com>
2022-06-13 18:34:12 +0200

    Fix fish bash imports

--- files/fish/.config/fish/from_bash.fish
+++ files/fish/.config/fish/from_bash.fish
@@ -1,21 +1,5 @@
 # Reuse aliases and envs from bash config files
 function _from_bash_config
-	if status is-interactive
-		egrep "^alias " $argv[1] | while read e
-		    set -l var (echo $e | sed -E "s/^alias ([A-Za-z0-9_-]+)=(.*)\$/\1/")
-		    set -l value (echo $e | sed -E "s/^alias ([A-Za-z0-9_-]+)=(.*)\$/\2/")
-		
-		    # remove surrounding quotes if existing
-		    set -l value (echo $value | sed -E "s/^\"(.*)\"\$/\1/")
-		
-		    # evaluate variables. we can use eval because we most likely just used "$var"
-		    set -l value (eval echo $value)
-		
-		    # set an alias
-		    alias $var="$value"
-		end
-	end
-	
 	egrep "^export " $argv[1] | while read e
 	    set -l var (echo $e | sed -E "s/^export ([A-Z0-9_]+)=(.*)\$/\1/")
 	    set -l value (echo $e | sed -E "s/^export ([A-Z0-9_]+)=(.*)\$/\2/")
@@ -45,6 +29,22 @@ function _from_bash_config
 	            # default
 	            set -xg $var $value
 	        end
+	end
+
+	if status is-interactive
+		egrep "^alias " $argv[1] | while read e
+		    set -l var (echo $e | sed -E "s/^alias ([A-Za-z0-9_-]+)=(.*)\$/\1/")
+		    set -l value (echo $e | sed -E "s/^alias ([A-Za-z0-9_-]+)=(.*)\$/\2/")
+		
+		    # remove surrounding quotes if existing
+		    set -l value (echo $value | sed -E "s/^\"(.*)\"\$/\1/")
+		
+		    # evaluate variables. we can use eval because we most likely just used "$var"
+		    set -l value (eval echo $value)
+		
+		    # set an alias
+		    alias $var="$value"
+		end
 	end
 end
 
