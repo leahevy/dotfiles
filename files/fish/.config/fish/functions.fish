0941079e46e0f4f7f29da1d3b5a5b03b3069480b
Leah Lackner <leah.lackner+github@gmail.com>
2022-06-17 09:43:14 +0200

    Cleanup code of venv activation

--- files/fish/.config/fish/functions.fish
+++ files/fish/.config/fish/functions.fish
@@ -1,32 +1,76 @@
-function pyenv-activate
-    source "$HOME/.pyenv/versions/$argv[1]/bin/activate.fish"
-end
+function _virtualenv-find_activate_rec
+    set -l actfile "$argv[1]/.venv/bin/activate.fish"
+    if test -e "$actfile"
+        echo "$actfile"
+        return
+    end
 
-function virtualenv-activate
-    set in_venv "$(pyenv virtualenvs 2>/dev/null | cut -f 1 -d "(" | grep -v "/" | grep -v "*" | grep "$(echo "$PWD/" | tr "/" "#" | tr " " "-" )" | head -n 1 | xargs)"
-    if test "$in_venv" != ""
-        pyenv-activate "$in_venv"
-        set -gx VENV "$in_venv"
-    else if test "$VENV" != ""
-        pyenv deactivate 2>&1 >/dev/null
-        set -gx VENV ""
+    set -l newdir "$(dirname "$argv[1]")"
+
+    if [ "$newdir" = "/" ]
+        return
+    else
+        _virtualenv-find_activate_rec "$newdir"
     end
 end
 
-if status is-interactive
-    virtualenv-activate 2>&1 >/dev/null
+function _virtualenv-find_activate
+    _virtualenv-find_activate_rec "$PWD"
+end
+
+function virtualenv-activate
+    set -l actfile "$(_virtualenv-find_activate)"
+    if [ "$actfile" != "" ]
+        set -gx VENV "$actfile"
+        set_color green
+        echo "++ Activate Python virtualenv ++"
+        set_color normal
+        source "$actfile" || true
+    else
+        set_color red
+        echo "++ .venv directory not found. Try 'virtualenv-create' ++" >&2
+        set_color normal
+    end
+end
+
+function virtualenv-deactivate
+	if [ "$VENV" != "" ]
+        set_color red
+        echo "++ Deactivate Python virtualenv ++"
+        set_color normal
+        set -gx VENV ""
+        deactivate 2>/dev/null >/dev/null || true
+    end
+end
+
+function _virtualenv-on_pwd
+    set -l actfile "$(_virtualenv-find_activate)"
+    if [ "$actfile" != "" ]
+        if [ "$actfile" != "$VENV" ]
+            virtualenv-activate
+        end
+    else
+        if [ "$VENV" != "" ]
+            virtualenv-deactivate
+        end
+    end
 end
 
 function __virtualenv-activate --on-variable PWD
   status --is-command-substitution; and return
-    virtualenv-activate 2>&1 >/dev/null
+    _virtualenv-on_pwd
 end
 
-function virtualenv-init
-    if test "$VENV" != ""
-        pyenv deactivate 2>&1 >/dev/null
+function virtualenv-create
+    if test -d "./.venv"
+        set_color red
+        echo "++ .venv directory already exists ++" >&2
+        set_color normal
+    else
+	    python -m venv .venv
     end
-    set -gx VENV "$(echo "$PWD" | tr "/" "#" | tr " " "-" )#"
-    pyenv virtualenv "$VENV"
-    pyenv-activate "$VENV"
+end
+
+if status is-interactive
+    virtualenv-activate 2>/dev/null >/dev/null
 end
