3bf573dd138000b2f8f2a3e40109d7bce46cecb7
Leah Lackner <leah.lackner+github@gmail.com>
2022-06-24 00:33:36 +0200

    Configure tmux-powerline (2)

--- files/tmux/.tmux-powerline-themes.d/mytheme.sh
+++ files/tmux/.tmux-powerline-themes.d/mytheme.sh
@@ -52,17 +52,17 @@ fi
 
 if [ -z $TMUX_POWERLINE_LEFT_STATUS_SEGMENTS ]; then
     TMUX_POWERLINE_LEFT_STATUS_SEGMENTS=(
-        #"tmux_session_info 148 234" \
+        "tmux_session_info 30 255" \
         "hostname 148 234" \
         #"ifstat 30 255" \
         #"ifstat_sys 30 255" \
         #"lan_ip 24 255 ${TMUX_POWERLINE_SEPARATOR_RIGHT_THIN}" \
         #"wan_ip 24 255" \
         "vcs_branch 33 0" \
-        #"vcs_compare 60 255" \
-        #"vcs_staged 64 255" \
-        #"vcs_modified 9 255" \
-        #"vcs_others 245 0" \
+        "vcs_compare 60 255" \
+        "vcs_staged 64 255" \
+        "vcs_modified 9 255" \
+        "vcs_others 245 0" \
     )
 fi
 
