f69bd86e8e0cc4629496257a952f3008691e33ae
Leah Lackner <leah.lackner+github@gmail.com>
2022-06-24 18:04:16 +0200

    Fix from_bash for MacOs (damn those missing GNU coreutils)

--- files/fish/.config/fish/fish/00-from-bash.fish
+++ files/fish/.config/fish/fish/00-from-bash.fish
@@ -1,71 +1,75 @@
 # Functions
 #----------
 function convert-bash-to-fish
-    set -l SED "/usr/bin/sed"
+    if [ -e "$HOMEBREW_PATH/gsed" ]
+        set _SED_CMD "$HOMEBREW_PATH/gsed"
+    else
+        set _SED_CMD "/usr/bin/sed"
+    end
 
     set -l preprocessed "$(echo "$argv[1]" | while read lineorig
-        set -l line "$(echo "$lineorig" | $SED -E 's/[$]([^(])/$$\1/g' | sed -E 's/^\s*(.*)\s*/\1/')"
+        set -l line "$(echo "$lineorig" | $_SED_CMD -E 's/[$]([^(])/$$\1/g' | $_SED_CMD -E 's/^\s*(.*)\s*/\1/')"
 	    switch $line
 	        case 'export *=*'
-		        set -l var (echo $line | $SED -E "s/^export ([A-Za-z0-9_-]+)=(.*)\$/\1/")
-		        set -l value (echo $line | $SED -E "s/^export ([A-Za-z0-9_-]+)=(.*)\$/\2/")
+		        set -l var (echo $line | $_SED_CMD -E "s/^export ([A-Za-z0-9_-]+)=(.*)\$/\1/")
+		        set -l value (echo $line | $_SED_CMD -E "s/^export ([A-Za-z0-9_-]+)=(.*)\$/\2/")
 
-                set -l value (echo $value | $SED -E "s/^\"(.*)\"\$/\1/")
-                set -l value (echo $value | $SED -E "s/^'(.*)'\$/\1/")
-                set -l value (echo $value | $SED -E 's/[$][$]/$/g')
+                set -l value (echo $value | $_SED_CMD -E "s/^\"(.*)\"\$/\1/")
+                set -l value (echo $value | $_SED_CMD -E "s/^'(.*)'\$/\1/")
+                set -l value (echo $value | $_SED_CMD -E 's/[$][$]/$/g')
 
 	            # replace ":" by spaces. this is how PATH looks for Fish
 	            if test $var = "PATH"
-	                set value "$(echo $value | $SED -E "s/:/\" \"/g")"
+	                set value "$(echo $value | $_SED_CMD -E "s/:/\" \"/g")"
                     echo set -gx "$var" "\"$value\";"
                 else
                     echo set -gx "$var" "\"$value\";"
 	            end
 	        case 'export *'
-		        set -l var (echo $line | $SED -E "s/^export ([A-Za-z0-9_-]+)\$/\1/")
+		        set -l var (echo $line | $_SED_CMD -E "s/^export ([A-Za-z0-9_-]+)\$/\1/")
                 echo set -gx "$var";
 	        case 'alias *=*'
-		        set -l var (echo $line | $SED -E "s/^alias ([A-Za-z0-9_-]+)=(.*)\$/\1/")
-		        set -l value (echo $line | $SED -E "s/^alias ([A-Za-z0-9_-]+)=(.*)\$/\2/")
+		        set -l var (echo $line | $_SED_CMD -E "s/^alias ([A-Za-z0-9_-]+)=(.*)\$/\1/")
+		        set -l value (echo $line | $_SED_CMD -E "s/^alias ([A-Za-z0-9_-]+)=(.*)\$/\2/")
 
-                set -l value (echo $value | $SED -E "s/^\"(.*)\"\$/\1/")
-                set -l value (echo $value | $SED -E "s/^'(.*)'\$/\1/")
-                set -l value (echo $value | $SED -E 's/[$][$]/$/g')
+                set -l value (echo $value | $_SED_CMD -E "s/^\"(.*)\"\$/\1/")
+                set -l value (echo $value | $_SED_CMD -E "s/^'(.*)'\$/\1/")
+                set -l value (echo $value | $_SED_CMD -E 's/[$][$]/$/g')
 
                 echo alias "$var='$value';"
 	        case 'source *'
-	            set -l value (echo $line | $SED -E 's/^source\\s*(.*)/\1/' | sed -E "s#~#$HOME#g")
+	            set -l value (echo $line | $_SED_CMD -E 's/^source\\s*(.*)/\1/' | $_SED_CMD -E "s#~#$HOME#g")
 
-                set -l value (echo $value | $SED -E "s/^\"(.*)\"\$/\1/")
-                set -l value (echo $value | $SED -E "s/^'(.*)'\$/\1/")
-                set -l value (echo $value | $SED -E 's/[$][$]/$/g')
+                set -l value (echo $value | $_SED_CMD -E "s/^\"(.*)\"\$/\1/")
+                set -l value (echo $value | $_SED_CMD -E "s/^'(.*)'\$/\1/")
+                set -l value (echo $value | $_SED_CMD -E 's/[$][$]/$/g')
 
                 echo eval-bash-file "$value";
 	        case 'if [[ *'
-	            set -l val (echo $line | $SED -E 's/^if \[\[(.*)\]\]\\s*;\\s*then\\s*/if [ \1 ]/')
-	            set -l val (echo $val | $SED -E 's/^if \[\[(.*)\]\]\\s*/if [ \1 ]/')
+	            set -l val (echo $line | $_SED_CMD -E 's/^if \[\[(.*)\]\]\\s*;\\s*then\\s*/if [ \1 ]/')
+	            set -l val (echo $val | $_SED_CMD -E 's/^if \[\[(.*)\]\]\\s*/if [ \1 ]/')
                 echo "$val"
 	        case 'if [ *'
-	            set -l val (echo $line | $SED -E 's/^if \[(.*)\]\\s*;\\s*then\\s*/if [ \1 ]/')
+	            set -l val (echo $line | $_SED_CMD -E 's/^if \[(.*)\]\\s*;\\s*then\\s*/if [ \1 ]/')
                 echo "$val"
 	        case 'if *'
-	            set -l val (echo $line | $SED -E 's/^if (.*)\\s*;\\s*then\\s*/if \1/')
+	            set -l val (echo $line | $_SED_CMD -E 's/^if (.*)\\s*;\\s*then\\s*/if \1/')
                 echo "$val"
 	        case '*()*{'
-                set -l val (echo $line | $SED -E 's/^(.*)\(\)\\s*\{\\s*/function \1/')
+                set -l val (echo $line | $_SED_CMD -E 's/^(.*)\(\)\\s*\{\\s*/function \1/')
                 echo "$val"
 	        case '}'
                 echo end
 	        case 'for *'
-                set -l val (echo $line | $SED -E 's/^for (.*)\\s*;\\s*do\\s*/for \1/')
+                set -l val (echo $line | $_SED_CMD -E 's/^for (.*)\\s*;\\s*do\\s*/for \1/')
                 echo "$val"
 	        case '*=*'
-		        set -l var (echo $line | $SED -E "s/^([A-Za-z0-9_-]+)=(.*)\$/\1/")
-		        set -l value (echo $line | $SED -E "s/^([A-Za-z0-9_-]+)=(.*)\$/\2/")
+		        set -l var (echo $line | $_SED_CMD -E "s/^([A-Za-z0-9_-]+)=(.*)\$/\1/")
+		        set -l value (echo $line | $_SED_CMD -E "s/^([A-Za-z0-9_-]+)=(.*)\$/\2/")
 
-                set -l value (echo $value | $SED -E "s/^\"(.*)\"\$/\1/")
-                set -l value (echo $value | $SED -E "s/^'(.*)'\$/\1/")
-                set -l value (echo $value | $SED -E 's/[$][$]/$/g')
+                set -l value (echo $value | $_SED_CMD -E "s/^\"(.*)\"\$/\1/")
+                set -l value (echo $value | $_SED_CMD -E "s/^'(.*)'\$/\1/")
+                set -l value (echo $value | $_SED_CMD -E 's/[$][$]/$/g')
 
                 echo set "$var \"$value\";"
 	        case 'do'
@@ -90,23 +94,23 @@ function convert-bash-to-fish
     end 2>&1)"
 
     echo "$preprocessed" | while read lineorig
-        set -l line "$(echo "$lineorig" | $SED -E 's/[$][$]/$/g')"
+        set -l line "$(echo "$lineorig" | $_SED_CMD -E 's/[$][$]/$/g')"
 
-        set line "$(echo $line | $SED -E 's/\[  ! -z "\$PS1"  \]/status --is-interactive/')"
+        set line "$(echo $line | $_SED_CMD -E 's/\[  ! -z "\$PS1"  \]/status --is-interactive/')"
 
-        set line "$(echo $line | $SED -E 's/"\$@"/$argv/')"
+        set line "$(echo $line | $_SED_CMD -E 's/"\$@"/$argv/')"
 
-        set line "$(echo $line | $SED -E 's/\$0/$(status filename)/')"
+        set line "$(echo $line | $_SED_CMD -E 's/\$0/$(status filename)/')"
 
-        set line "$(echo $line | $SED -E 's/\$1/$argv[1]/')"
-        set line "$(echo $line | $SED -E 's/\$2/$argv[2]/')"
-        set line "$(echo $line | $SED -E 's/\$3/$argv[3]/')"
-        set line "$(echo $line | $SED -E 's/\$4/$argv[4]/')"
-        set line "$(echo $line | $SED -E 's/\$5/$argv[5]/')"
-        set line "$(echo $line | $SED -E 's/\$6/$argv[6]/')"
-        set line "$(echo $line | $SED -E 's/\$7/$argv[7]/')"
-        set line "$(echo $line | $SED -E 's/\$8/$argv[8]/')"
-        set line "$(echo $line | $SED -E 's/\$9/$argv[9]/')"
+        set line "$(echo $line | $_SED_CMD -E 's/\$1/$argv[1]/')"
+        set line "$(echo $line | $_SED_CMD -E 's/\$2/$argv[2]/')"
+        set line "$(echo $line | $_SED_CMD -E 's/\$3/$argv[3]/')"
+        set line "$(echo $line | $_SED_CMD -E 's/\$4/$argv[4]/')"
+        set line "$(echo $line | $_SED_CMD -E 's/\$5/$argv[5]/')"
+        set line "$(echo $line | $_SED_CMD -E 's/\$6/$argv[6]/')"
+        set line "$(echo $line | $_SED_CMD -E 's/\$7/$argv[7]/')"
+        set line "$(echo $line | $_SED_CMD -E 's/\$8/$argv[8]/')"
+        set line "$(echo $line | $_SED_CMD -E 's/\$9/$argv[9]/')"
 
         echo "$line"
     end
@@ -121,15 +125,15 @@ function convert-bash-to-fish-file
     # Cached result exists
     if [ -e "/tmp/fishcache/$argv[1]" ]
         # Check if source file has changed
-        if [ "$(stat -c %Z "$argv[1]")" != "$(cat "/tmp/fishcache/$argv[1]._STAT")" ]
+        if [ "$(md5sum "$argv[1]" | cut -f1 -d " ")" != "$(cat "/tmp/fishcache/$argv[1]._STAT")" ]
             set RECREATE "true"
         end
     else
         set RECREATE "true"
     end
     if [ "$RECREATE" = "true" ]
-        mkdir -p "$(dirname "/tmp/fishcache/$argv[1]")"
-        stat -c %Z "$argv[1]" > "/tmp/fishcache/$argv[1]._STAT"
+        mkdir -p "$(dirname "/tmp/fishcache/$argv[1]")" >/dev/null
+        md5sum "$argv[1]" | cut -f1 -d " " > "/tmp/fishcache/$argv[1]._STAT"
         convert-bash-to-fish "$(cat "$argv[1]")" > "/tmp/fishcache/$argv[1]"
     end
     cat "/tmp/fishcache/$argv[1]"
