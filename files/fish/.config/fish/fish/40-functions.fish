534053fd24bd7d2be3a37cbca604288809012ac0
Leah Lackner <leah.lackner+github@gmail.com>
2022-06-24 22:16:23 +0200

    Fix fish-reload

--- files/fish/.config/fish/fish/40-functions.fish
+++ files/fish/.config/fish/fish/40-functions.fish
@@ -1,6 +1,13 @@
 function fish-reload
-    /usr/bin/rm -rf /tmp/fishcache
+    /bin/rm -rf /tmp/fishcache
     clear
+    for file in $(ls "$HOME/.config/fish/fish")
+        source "$HOME/.config/fish/fish/$file"
+    end
+end
+
+function fish-reload-noclear
+    /bin/rm -rf /tmp/fishcache
     for file in $(ls "$HOME/.config/fish/fish")
         source "$HOME/.config/fish/fish/$file"
     end
